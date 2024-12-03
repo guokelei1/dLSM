f9e159d78f380d72b81ba5f46b851572cb39d6a6
gkl <2652313711@qq.com>
2024-12-03 17:56:35 +0800

    first try

--- server.cc
+++ server.cc
@@ -28,7 +28,7 @@ int main(int argc,char* argv[])
      mn_keeper = new dLSM::Memory_Node_Keeper(true, tcp_port, pr_size);
      dLSM::RDMA_Manager::node_id = 2* Memory_server_id;
   }else{
-    mn_keeper = new dLSM::Memory_Node_Keeper(true, 19843, 88);
+    mn_keeper = new dLSM::Memory_Node_Keeper(true, 19843, 32);
     dLSM::RDMA_Manager::node_id = 0;
   }
 
